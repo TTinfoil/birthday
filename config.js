e4e17372c1dc30bb970feea33ad36a4de8bcc9a0
TTinfoil <121388980+TTinfoil@users.noreply.github.com>
2022-12-25 22:46:23 +0800

    Update config.js

--- config.js
+++ config.js
@@ -41,13 +41,13 @@ var config = {
     },
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
-        turn_on: "开始",
-        play: "音乐",
-        bannar_coming: "颜色",
-        balloons_flying: "好像少点东西",
-        cake_fadein: "蛋糕？",
-        light_candle: "蜡烛？",
-        wish_message: "生日快乐",
-        story: "A MESSAGE FOR YOU",
+        开始: "开始",
+        音乐: "音乐",
+        颜色: "颜色",
+        好像少点东西: "好像少点东西",
+        蛋糕？: "蛋糕？",
+        蜡烛？: "蜡烛？",
+        生日快乐: "生日快乐",
+        A MESSAGE FOR YOU: "A MESSAGE FOR YOU",
     }
 };
